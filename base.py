0765361318ef30819b8b4220f4b827cd3d5c9fdf
DevCairo <abdallahhaziz@icloud.com>
2021-05-13 01:23:33 -0600

    Compiled program for windows..

--- base.py
+++ base.py
@@ -1,9 +1,10 @@
 from zipfile import ZIP_DEFLATED, ZipFile
-from platform import system
-from urllib import request
+import platform
 import argparse
 import requests
+import getpass
 import os
+import io
 
 
 class Base():
@@ -17,39 +18,52 @@ class Base():
         '-r', '--remove', help="Remove a package from your system")
     args = parser.parse_args()
 
-
     if args.add:
-            repo = args.add
+        zipfile = args.add
+        try:
+            os.environ["PROGRAMFILES(X86)"]
+            arch = "x64"
+        except:
+            arch = "x32"
+        if platform.system() == "Darwin":
+            url = f"https://mirrors.devcairo.xyz/macos/{zipfile}.zip"
             try:
-                os.environ["PROGRAMFILES(X86)"]
-                arch = "x64"
-            except:
-                arch = "x32"
-            if system() == "Windows":
-                url = f"https://mirrors.devcairo.xyz/windows/{arch}/{repo}.zip"
-                file = requests.get(url)
-                zipf = ZipFile(file, 'r')
-                ff = zipf.namelist()[0]
-                deff = zipf.open(ff)
-                content = deff.read()
-            elif system() == "Darwin":
-                url = f"https://mirrors.devcairo.xyz/macos/{repo}.zip"
-                file = requests.get(url)
-                zipf = ZipFile(file, 'r')
-                ff = zipf.namelist()[0]
-                deff = zipf.open(ff)
-                content = deff.read()
-            elif system() == "Linux":
-                url = f"https://mirrors.devcairo.xyz/linux/{arch}/repo.zip"
-                file = requests.get(url)
-                zipf = ZipFile(file, 'r')
-                ff = zipf.namelist()[0]
-                deff = zipf.open(ff)
-                content = deff.read()
-                zipf.close()
-
-            
-
+                url = f"https://mirrors.devcairo.xyz/macos/{zipfile}.zip"
+                r = requests.get(url)
+                if r.ok:
+                    print("[ OK ] Connected to mirrors.devcairo.xyz and beginning download!")
+                    os.chdir(f'/Users/{getpass.getuser()}')
+                    z = ZipFile(io.BytesIO(r.content))
+                    newdir = os.mkdir(f'/Users/{getpass.getuser()}/dcpd/{zipfile}')
+                    z.extractall(f'/Users/{getpass.getuser()}/dcpd/{zipfile}')
+            except Exception as e:
+                print(f"[ ERR ] {e}")
+        elif platform.system() == "Windows":
+            try:
+                url = f"https://mirrors.devcairo.xyz/windows/{arch}/{zipfile}.zip"
+                r = requests.get(url)
+                if r.ok:
+                    print("[ OK ] Connected to mirrors.devcairo.xyz and beginning download!")
+                    os.chdir(f'C:\\Users\\{getpass.getuser()}\\dcpd')
+                    z = ZipFile(io.BytesIO(r.content))
+                    newdir = os.mkdir(f'C:\\Users\\{getpass.getuser()}\\dcpd\\{zipfile}')
+                    z.extractall(f'C:\\Users\\{getpass.getuser()}\\dcpd\\{zipfile}')
+            except Exception as e:
+                print(f"[ ERR ] {e}")
+        elif platform.system() == "Linux":
+            url = f"https://mirrors.devcairo.xyz/linux/{arch}/{zipfile}.zip"
+            try:
+                url = f"https://mirrors.devcairo.xyz/linux/{arch}/{zipfile}.zip"
+                r = requests.get(url)
+                if r.ok:
+                    print("[ OK ] Connected to mirrors.devcairo.xyz and beginning download!")
+                    os.chdir(f'/home/{getpass.getuser()}')
+                    z = ZipFile(io.BytesIO(r.content))
+                    newdir = os.mkdir(f'/home/{getpass.getuser()}/dcpd/{zipfile}')
+                    z.extractall(f'/home/{getpass.getuser()}/dcpd/{zipfile}')
+            except Exception as e:
+                print(f"[ ERR ] {e}")
+        print("Retrieved important information..\nBeginning download ✔️")
 
 if __name__ == "__main__":
     Base()
